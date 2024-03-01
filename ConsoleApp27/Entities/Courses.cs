c0125ac09846005ffaab85cffc7886a5d82e542c
Victor R. Silva <victorr.silva005@gmail.com>
2024-03-01 17:39:36 -0300

    Finished program

---
+++ ConsoleApp27/Entities/Courses.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp27.Entities
+{
+    internal class Courses
+    {
+        public int Id { get; set; }
+
+        public Courses(int id)
+        {
+            Id = id;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if(!(obj is Courses))
+            {
+                return false;
+            }
+            Courses other = obj as Courses;
+            return Id.Equals(other.Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
---
+++ ConsoleApp27/Program.cs
@@ -0,0 +1,40 @@
+﻿using ConsoleApp27.Entities;
+using System.Collections.Generic;
+
+namespace ConsoleApp27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            HashSet<Courses> set = new HashSet<Courses>();
+            
+            for (int i = 0; i < 3; i++)
+            {
+                int result = default;
+                string input;
+                int counter = 65;
+
+                Console.Write("\nHow many students for course " + Convert.ToChar(65 + i) + " : ");
+                int students = int.Parse(Console.ReadLine());
+
+                for (int j = 0; j < students; j++)
+                {
+                    Console.Write("Enter the id of the student: ");
+                    input = Console.ReadLine();
+                    while (!int.TryParse(input, out result))
+                    {
+                        Console.Write("Invalid input, try again: ");
+                        input = Console.ReadLine();
+                    }
+
+                    set.Add(new Courses(result));
+
+                }
+            }
+
+            Console.WriteLine("\nTotal students: " + set.Count);
+        }
+    }
+}
